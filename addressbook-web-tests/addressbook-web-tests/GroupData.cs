3154055f11ad27ec386b7c6fbfaa7dbac5bdb118
Vladimir Zankov <vladimirzankov@outlook.com>
2018-08-17 12:40:47 +0300

    Created GroupData and AccountData classes

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string name;
+        private string header;
+        private string footer;
+
+        public GroupData(string name, string header, string footer)
+        {
+            this.name = name;
+            this.header = header;
+            this.footer = footer;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}
